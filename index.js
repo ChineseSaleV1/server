f9d31bf9e2cbb2d7fd30cc04561e19b52b1bb22f
fisher-chana <cf0556772995@gmail.com>
2022-03-27 16:22:30 +0300

    app page

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const items = require('./route/product')
+const users = require('./route/users')
+const winning = require('./route/winning')
+const app = express()
+mongoose.connect("mongodb://localhost:27017/chinaSale")
+    .then(() => { console.log("mongo db connected"); })
+    .catch(er => { console.log(er) });
+app.use(express.json())
+
+
+app.use("/product",productRoute)
+app.use("/user",userRoute)
+app.use("/win",winningRoute)
+
+app.listen(4500, () => console.log(`listening at http://localhost:4500`));
+
