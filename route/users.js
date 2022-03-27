7f2f3e109989c305aaf163c62f7a7d966c1d905d
fisher-chana <cf0556772995@gmail.com>
2022-03-27 16:42:48 +0300

    do some changes

--- route/users.js
+++ route/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/user");
+const { User } = require('../models/user.model');
 
 router.get('/:id', userController.getOrdersByUserId);
 router.post('', userController.addUser);
 router.post('', userController.addOrder);
 module.exports = router;
+console.log( User.addUser("moshe","green","0556772995",null));
